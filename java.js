cce52c76bfab4588d53a0124298479d9cf3b4069
nadim184 <51932411+nadim184@users.noreply.github.com>
2019-07-03 16:31:45 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,36 @@
+
+
+  /* var s="hello";
+    
+    document.getElementById('hi').addEventListener("mouseover",function(){
+        
+        setTimeout(() => {
+            document.getElementById('hi').innerHTML=s;
+        }, 20);
+
+    });
+    
+
+    document.getElementById('hi').addEventListener("mouseout",function(){
+       
+        setTimeout(() => {
+            document.getElementById('hi').innerHTML="Welcome";
+        }, 20);
+
+    });
+
+  */
+
+    $("#link").click(function(){
+        
+        $("#bx").css("visibility","visible",)  
+        
+        
+       
+    }); 
+    $("#butt").click(function(){
+        $("#bx").css("visibility","hidden");
+    })
+
+ 
+    
